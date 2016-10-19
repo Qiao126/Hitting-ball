22b752d11de816267ce8ed7d0b6f36a0520dae6c
Qiao Deng <dengqiao@1x-193-157-185-175.uio.no>
2016-10-19 15:20:59 +0200

    Assets

---
+++ Assets/Blinking.cs
@@ -0,0 +1,168 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using System.IO;
+
+public class Blinking : MonoBehaviour
+{
+    public Text display;
+    private bool isTargetOn;
+    public int total;
+    private int trial;
+    public int block;
+    public float distance;
+    public float width;
+    private double timer;
+    public int hits;
+    private GameObject[] walls;
+    private float flash;
+
+    public int mode;
+    private float speed_a;
+    private float speed_b;
+    private float current_speed;
+    private float enter_speed;
+    private bool hasEntered;
+    private string speed_report;
+
+    private int i, j, k;
+    float[] D = { 0.9f, 1.5f };
+    float[] W = { 0.08f, 0.16f, 0.24f };
+    int[] M = { 0, 1, 2, 3 };
+
+    // Use this for initialization
+    void Start()
+    {
+        isTargetOn = false;
+        trial = 1;
+        block = 1;
+        hasEntered = false;
+        flash = 1;
+        hits = 0;
+        i = 0;
+        j = 0;
+        k = 0;
+        distance = D[i];
+        width = W[j];
+        mode = k;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if (trial < total+1 && block < 25)
+        {
+            Blink();
+            this.display.text = "Block: " + block.ToString() + " Trial: " + trial.ToString() + " Count: " + hits.ToString();
+        } else {
+            block++;
+            trial = 1;
+            hits = 0;
+            k++;
+            if (k > 3) { k = 0; j++; }
+            if (j > 2) { j= 0; i++; }
+            distance = D[i];
+            width = W[j];
+            mode = k;
+        }
+        if (mode == 1 || mode == 3)
+        {
+            if (flash < 1)
+            {
+                flash = flash + 0.1f;
+            }
+            else
+            {
+                flash = 1;
+            }
+            walls = GameObject.FindGameObjectsWithTag("marked_wall");
+            foreach (GameObject wall in walls)
+            {
+                wall.GetComponent<MeshRenderer>().material.color = new Color(0.67f, 0.96f, 0.65f, flash);
+            }
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        enter_speed = other.attachedRigidbody.GetPointVelocity(this.gameObject.GetComponent<SphereCollider>().center).magnitude;
+
+        if (mode == 0)
+        {
+            if (other.gameObject.name == "myPalm")
+            {
+                
+                if (isTargetOn)
+                {
+                    hits++;
+                    print(hits + "," + trial);
+                    this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+                    flash = 0;
+                }
+            }
+        }
+        speed_a = enter_speed;
+        speed_b = enter_speed;
+        hasEntered = true;
+    }
+
+    void OnTriggerStay(Collider other)
+    {
+        if (mode == 1)
+        {
+            current_speed = other.attachedRigidbody.GetPointVelocity(this.gameObject.GetComponent<SphereCollider>().center).magnitude;
+            if (current_speed != 0 && hasEntered)
+            {
+                if (speed_a > speed_b && current_speed > speed_b)
+                {
+                    if (other.gameObject.name == "myPalm")
+                    {
+                        if (isTargetOn)
+                        {
+                            hits++;
+                            print(hits + "," + trial);
+                            speed_report += "\n" + 999999999999999;
+                            //print(speed_a +" , " + speed_b +" , "+current_speed);
+                            this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+                            flash = 0;
+                        }
+                        hasEntered = false;
+                    }
+                }
+                speed_a = speed_b;
+                speed_b = current_speed;
+            }
+
+            if (current_speed != 0)
+            {
+                speed_report += "\n" + current_speed;
+            }
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        File.WriteAllText("./report.txt", speed_report);
+    }
+
+    void Blink()
+    {
+        if (Time.time > timer)
+        {
+            if (isTargetOn)
+            {
+                timer = Time.time + distance;
+                isTargetOn = false;
+                this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(0.0f, 0.0f, 0.0f, 0.0f);
+            }
+            else
+            {
+                timer = Time.time + width;
+                isTargetOn = true;
+                this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                trial++;
+            }
+        }
+    }
+
+}
+
---
+++ Assets/boxer.cs
@@ -0,0 +1,122 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.IO;
+using UnityEngine.UI;
+
+public class boxer : MonoBehaviour {
+	private double timer;
+	private bool isTargetOn;
+	private int count;
+	private Vector3 ang_col;
+	private Vector3 ang_boxer;
+
+	private float speed_a;
+	private float speed_b;
+	private float current_speed;
+	private float enter_speed;
+	public bool hasEntered;
+    public bool hasHit;
+
+    public int mode;     // 0 no haptic   1 haptic
+    public int detection;   // 0 continuous dynamic         1 like a boxer
+    public Text display;
+    private float dist;
+    //	private Vector3 lastpos;
+    //	private Vector3 lastvel;
+    //	private Vector3 lastanvel;
+
+    // Use this for initialization
+    void Start () {
+		isTargetOn = true;
+        dist = 0;
+       // File.WriteAllText("./exp2/report.txt","");//clear all
+		hasEntered=false;
+        hasHit = false;
+	
+	}
+
+//	void FixedUpdate(){
+//		lastpos = this.gameObject.transform.position;
+//		lastvel = this.gameObject.GetComponent<Rigidbody> ().velocity;
+//		lastanvel = this.gameObject.GetComponent<Rigidbody> ().angularVelocity;
+//	}
+	// Update is called once per frame
+	void Update () {
+        if (count < 51)
+        {
+            this.display.text = "Trail: " + count + " Distance from center: " + this.dist.ToString("#.00");
+
+            if (!isTargetOn && Time.time > timer)
+            {
+
+                this.gameObject.GetComponent<MeshRenderer>().material.color = new Color(1.0f, 1.0f, 1.0f, 1.0f);
+                this.gameObject.transform.position = new Vector3(0.0f, 0.9f, 0.25f);
+                this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+                this.gameObject.GetComponent<Rigidbody>().detectCollisions = true;
+                isTargetOn = true;
+            }
+            if (detection == 1 && hasEntered && !hasHit)
+            {
+                this.gameObject.transform.position = new Vector3(0.0f, 0.9f, 0.25f);
+                this.gameObject.GetComponent<Rigidbody>().velocity = new Vector3(0, 0, 0);
+                this.gameObject.GetComponent<Rigidbody>().angularVelocity = new Vector3(0, 0, 0);
+            }
+        }
+    }
+
+	void OnCollisionEnter (Collision col)
+	{
+        
+		if (col.gameObject.name == "myPalm") {    //add LIKE_A_BOXER algorithm
+			
+			timer = Time.time + 1.5;
+			ang_col = col.contacts [0].normal;
+            //print ("angle_collision:"+ang_col.ToString());
+           // this.gameObject.GetComponent<Rigidbody>().AddForce(ang_col * 2, ForceMode.Impulse);
+            if (detection == 1)
+            {
+                enter_speed = col.gameObject.GetComponent<Rigidbody>().GetPointVelocity(this.gameObject.GetComponent<SphereCollider>().center).magnitude;
+                speed_a = enter_speed;
+                speed_b = enter_speed;
+                hasEntered = true;
+            }
+            else { isTargetOn = false; }
+		}
+		if(col.gameObject.name == "backwall")// 
+		{
+			this.gameObject.GetComponent<MeshRenderer> ().material.color = new Color (0.0f, 0.0f, 0.0f, 0.0f);
+			this.gameObject.GetComponent<Rigidbody> ().detectCollisions = false;
+			ContactPoint contact = col.contacts [0];
+			print(contact.point+" trial: "+count);
+			dist = Vector3.Distance (contact.point, col.gameObject.transform.position);
+            print(dist);
+			//File.AppendAllText("./exp2/report.txt",dist.ToString()+"\n");
+			isTargetOn = false;
+            hasHit = false;
+			count++;
+		}
+
+	}
+	void OnCollisionStay(Collision col){
+       
+            if (col.gameObject.name == "myPalm" && detection==1) {
+        
+                current_speed = col.gameObject.GetComponent<Rigidbody> ().GetPointVelocity (this.gameObject.GetComponent<SphereCollider> ().center).magnitude;
+			if (current_speed != 0 && hasEntered) {
+				if (speed_a > speed_b && current_speed > speed_b) {
+                    ang_boxer = col.contacts [0].normal;
+                    print(speed_a + " " + speed_b + " " + current_speed);
+                   // this.gameObject.GetComponent<Rigidbody> ().AddForce (ang_boxer*2, ForceMode.Impulse); //determine the directtion of ball! ang_col? ang_boxer? XXXXXXXXXXXXX hand.normal
+                    hasHit = true;
+					hasEntered = false;
+				} 
+				speed_a = speed_b;
+				speed_b = current_speed;
+			}
+
+			isTargetOn = false;
+		}
+	}
+
+}
